395ea4b1710a9c945677b818dc29dfde0054feb9
Tevfik Enes Özel (ETIYA) <enesozel.eo@gmail.com>
2019-03-10 05:00:33 +0300

    mongoose installation

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
 
 const app = express();
 require("./routes/authRoutes")(app);
 //** app.get("/", (req, res) => {res.send({ bye:'buddy' });});
-
-//http://console.developer.google.com/
+mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-//http://localhost:5000/auth/google/callback?code=4/CQGRBm_NUSUgUC2fQXUdKLo0txhM9Odq3iwPGNBI14VTkeCHShtHEYuunhtWhSP7UO0JcqX8d0_1GvnRqEKULEw&scope=email%20profile%20https://www.googleapis.com/auth/userinfo.profile%20https://www.googleapis.com/auth/userinfo.email
